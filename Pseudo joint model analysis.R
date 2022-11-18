6cfb4ca03be6b59fc288db7d336d6c3d10239035
Annah92 <85847863+Annah92@users.noreply.github.com>
2022-11-18 02:21:20 +0000

    Update Pseudo joint model analysis.R

--- Pseudo joint model analysis.R
+++ Pseudo joint model analysis.R
@@ -43,37 +43,49 @@ dim(SurvData)
 ## Weibull baseline hazard
 ## h0 = lambda*rho*t^(rh0-1)   and   H0 = lambda*t^(rho)
 ## ---------------------------------------------------------------------
-likelihood.Joint<-function(p)
+  likelihood.Joint<-function(p)
   { 
-  ids <- SurvData$id
-  n_i <- length(unique(ids))
-  G<-length(unique(LongData$pair)) # #G: Total number of clusters
-  nij <- as.vector(tapply(ids, ids, length)) # same as as.vector(table(long$id))  # #nij: Number of measurements per individual
-  nij1j2 <-as.vector(table(LongData$pair))
-  H_it<- rep(NA, n_i)
-  likk<- rep(NA, G)
-lambda       <-  exp(p[1] )
-rho      <-exp( p[2] )
-theta <- exp(p[3]) # 
-alpha  <- p[4]
-sigma_u <-  exp(p[5]  )
-sigma_e<-  exp( p[6]  )
-Hit<-rep(NA, n_i)
-for(i in 1:G) ## s<-3
-{
-  likk[i]<-adaptIntegrate( function(uv)  as.numeric( sapply( split( ( ( lambda*rho*SurvData$ATF^(rho-1)* uv[2]*exp(  alpha*(SurvData$bij+uv[1]+Beta1*SurvData$ATF )  ) )^SurvData$status )*
- (for(id in 1:n_i){ Hit[id]<-
- integrate(function(s)
- lambda*rho*s^(rho-1)*uv[2]* exp( alpha*(Beta0+SurvData$bij[id]+uv[1]+Beta1*s) ),
-  0, SurvData$ATF[id] )$value
-}) , SurvData$pair),prod) *((2*pi*(sigma_e^2) )^(-nij1j2/2) * exp(   -1/(2*(sigma_e^2)) * 
- aggregate((LongData$zij-Beta0-LongData$bij-uv[1]-Beta1*LongData$start)^2 , by=list ( LongData$pair ) ,FUN=sum)[,2]  )) ) [i]*
-  dgamma(x=uv[2],shape=1/theta, scale =theta )* dnorm(uv[1],mean=0,sd=sigma_u )  , 
- lowerLimit= c(-20,0 ) , upperLimit= c(20, 100) )[1]
-}
-lik<-sum(log( unlist( likk ) ) )
--lik
-}
-initial2<-c(log(0.01), log(2), log(0.5), -3.5,log(0.09),log(0.04))
-t2<-nlminb(initial2,likelihood.Joint, control=list(trace=TRUE))
-t2
+    lambda       <-  exp(p[4] )
+    rho      <- exp( p[6] )
+    theta <- exp(p[2] ) # s=log(var.eps) <=> exp(s)=var.eps
+    alpha  <- p[3]
+    sigma_u <- exp( p[1]   )
+    sigma_e <-  exp( p[5]  )
+    n <- length(unique(LongData$pair))
+    L.ni <-  as.vector(table(LongData$pair))
+    yit<- LongData$zij-Beta0-LongData$bij-Beta1*LongData$start 
+    L.yit<-split(yit, LongData$pair )
+    S.status<- aggregate( SurvData$status  , by =  list(SurvData$pair)  ,FUN=sum )[2] 
+    hit<- ( (lambda*rho*SurvData$ATF^(rho-1) )* exp(  alpha*(Beta0+SurvData$bij+Beta1*SurvData$ATF )  ) )^SurvData$status
+    S.hit<-split(hit, SurvData$pair )
+    S.ni<-as.vector(table(SurvData$pair))
+    Hit<- unlist (sapply(1:nrow(SurvData),   function(i)  adaptIntegrate( function(s)
+      exp(alpha*(Beta0 + SurvData$bij[i] ) )* s^(rho-1)* exp(alpha*Beta1*s) ,lowerLimit= c(0) , upperLimit= c( SurvData$ATF[i]) )[1]),
+      use.names=FALSE)
+    S.Hit<-split(Hit, SurvData$pair )
+    H0it<- unlist (sapply(1:nrow(SurvData),   function(i)  adaptIntegrate( function(s)
+      exp(alpha*(Beta0 + SurvData$bij[i] ) )* s^(rho-1)* exp(alpha*Beta1*s) ,lowerLimit= c(0) , upperLimit= c( SurvData$A0[i]) )[1]),
+      use.names=FALSE)
+    S0.Hit<-split(H0it, SurvData$pair )
+    
+    lik<-sum(log( unlist( 
+      sapply(
+        1:n, function(i)
+          adaptIntegrate( function(uv)
+            as.numeric(      ( (2*pi*(sigma_e^2 ) )^(-L.ni[i]/2) * exp(   -1/(2*(sigma_e^2 )  ) *  sum( (L.yit[[i]] - uv[1])^2  )  ) )*
+                               ( prod( S.hit[[i]] )*( exp(alpha*uv[1]) )^S.status[i,]    )*
+                               (( ( 1/theta+ sum(S0.Hit[[i]])*lambda*rho*exp(alpha*uv[1]) )^(  1/theta  ) )/ gamma(1/theta) )*
+                               ( gamma( S.status[i,] + 1/theta ) /( ( 1/theta+ sum(S.Hit[[i]])*lambda*rho*exp(alpha*uv[1]) )^( S.status[i,] + 1/theta  ) ) ) )* 
+              #   ( 1/(sqrt(2*pi* (sigma_u^2) ) ) )*exp( -uv[1]^2/(2*(sigma_u^2) )),
+              dnorm(uv[1],mean=0,sd=sigma_u ) , 
+            lowerLimit= c(-10 ) , upperLimit= c(10) )[1]
+      )                                        
+    ) ) )
+    -lik
+  }
+  initial2<-c( log(0.1), log(0.5), 2,log(0.001) , log(0.6), log(2) )
+  system.time(t2<- nlminb(initial2, likelihood.Joint, gradient = NULL, hessian = NULL,
+                          control = list(trace=T,  rel.tol = 1e-6, step.min= 1e-3)) )
+  cbind(sigma_u_JM = exp(t2$par[1]), Theta_JM=exp(t2$par[2]),
+        alpha_JM= t2$par[3], Lambda_JM=exp(t2$par[4]),
+        sigma_e_JM=exp(t2$par[5]),Rho_JM= exp(t2$par[6]) )
